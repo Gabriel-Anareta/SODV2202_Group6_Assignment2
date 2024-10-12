7a50062e56053d3c6ae51db4e3e0203fdfa3152c
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-12 09:55:38 -0600

    Implemented basic csv file reading

--- NHLPlayers/PlayerData.cs
+++ NHLPlayers/PlayerData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,36 @@ namespace NHLPlayers
 {
     static public class PlayerData
     {
+        static public List<string> AllData = GetData(@"NHL Player Stats 2017-18.csv");
+
+        static public List<string> GetData(string path)
+        {
+            List<string> data = new List<string>();
+            string csvPath = Path.Combine(Environment.CurrentDirectory, path);
+
+            if (File.Exists(csvPath))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(csvPath))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            data.Add(reader.ReadLine());
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    // Catch error in reading file -- Place code here to display an error message
+                }
+            }
+            else
+            {
+                // Catch exception in invalid file path -- Place code here to display an error message
+            }
+
+            return data;
+        }
     }
 }
