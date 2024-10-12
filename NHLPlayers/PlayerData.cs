0435716db1309d42ccd25f6596da1c6a30e54767
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-12 09:44:56 -0600

    Created PlayerData class

---
+++ NHLPlayers/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHLPlayers
+{
+    static public class PlayerData
+    {
+    }
+}
