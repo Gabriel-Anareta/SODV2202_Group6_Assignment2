32bda8d3e7ccbbfaccc0b237fb81855590b0f197
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-12 10:37:19 -0600

    Created constructor for Player

--- NHLPlayers/Player.cs
+++ NHLPlayers/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NHLPlayers
 {
@@ -29,5 +30,23 @@ namespace NHLPlayers
         public string TOI_GP { get; set; }      // TOI/GP changed to TOI_GP
         public string Shifts_GP { get; set; }   // Shifts/GP changed to Shifts_GP
         public string FOW_Perc { get; set;  }   // FOW% changed to FOW_Perc 
+
+        public Player(
+            string _Name,           string _Plus_Minus,         string _GWG,
+            string _Team,           string _PIM,                string _OTG,
+            string _Pos,            string _P_GP,               string _SOG,
+            string _GP,             string _PPG,                string _S_Perc,
+            string _G,              string _PPP,                string _TOI_GP,
+            string _A,              string _SHG,                string _Shifts_GP,
+            string _PTS,            string _SHP,                string _FOW_Perc)
+        {
+            Name = _Name;           Plus_Minus = _Plus_Minus;   GWG = _GWG;
+            Team = _Team;           PIM = _PIM;                 OTG = _OTG;
+            Pos = _Pos;             P_GP = _P_GP;               SOG = _SOG;      
+            GP = _GP;               PPG = _PPG;                 S_Perc = _S_Perc;   
+            G = _G;                 PPP = _PPP;                 TOI_GP = _TOI_GP;   
+            A = _A;                 SHG = _SHG;                 Shifts_GP = _Shifts_GP;
+            PTS = _PTS;             SHP = _SHP;                 FOW_Perc = _FOW_Perc; 
+        }
     }
 }
