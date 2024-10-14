ea23eb3a5247dc726cadb0c06344201fe9e6437e
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-14 15:46:39 -0600

    Created GetExpressions method

--- NHLPlayers/ExpressionManager.cs
+++ NHLPlayers/ExpressionManager.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace NHLPlayers
 {
     public static class ExpressionManager
     {
+        // (\w|[+\-/%])+[<>=]{1,2}(\w|[\-\.])+      :   full ex
+        // ^(\w|[+\-/%])+                           :   prop
+        // [<>=]{1,2}                               :   op
+        // (\w|[\-\.])+$                            :   arg
+
+        public static MatchCollection GetExpressions(string input)
+        {
+            Regex filter = new Regex(@"(\w|[+\-/%])+[<>=]{1,2}(\w|[\-\.])+", RegexOptions.Compiled);
+
+            return filter.Matches(input);
+        }
     }
 }
