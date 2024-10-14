e27164ba512f67a691a2f745b768400723dc32ec
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-14 14:03:49 -0600

    Implemented CustomTime class

--- NHLPlayers/CustomTime.cs
+++ NHLPlayers/CustomTime.cs
@@ -8,5 +8,18 @@ namespace NHLPlayers
 {
     internal class CustomTime
     {
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+
+        public CustomTime(int minutes, int seconds)
+        {
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public override string ToString()
+        {
+            return $"{Minutes}:{Seconds}";
+        }
     }
 }
