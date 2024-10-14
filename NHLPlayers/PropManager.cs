7693bb61182babecdcea1bc4753292aee21e5137
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-14 13:59:16 -0600

    Created GetPropValue method

--- NHLPlayers/PropManager.cs
+++ NHLPlayers/PropManager.cs
@@ -9,6 +9,18 @@ namespace NHLPlayers
 {
     public static class PropManager
     {
-        
+        public static object GetPropValue(Object obj, string prop)
+        {
+            Type type = obj.GetType();
+            PropertyInfo[] props = type.GetProperties();
+
+            if (type.GetProperty(prop) == null)
+                return new { value = "invalid" };
+
+            if (!props.Contains(type.GetProperty(prop)))
+                return new { value = "invalid" }; ;
+
+            return new { value = type.GetProperty(prop).GetValue(obj) };
+        }
     }
 }
