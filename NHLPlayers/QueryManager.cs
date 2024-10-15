940f3680cc93ec504e42093a46dcccef80a9c44f
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-14 23:37:39 -0600

    Fixed bug with casting to CustomTime

--- NHLPlayers/QueryManager.cs
+++ NHLPlayers/QueryManager.cs
@@ -9,6 +9,7 @@ namespace NHLPlayers
 {
     public static class QueryManager
     {
+        // method uses dynamic types - make sure checking is done extensively to avoid type errors
         public static bool RunFilters(MatchCollection rawfilters, Object obj)
         {
             bool state = true;
@@ -16,23 +17,20 @@ namespace NHLPlayers
             foreach (Match filter in rawfilters)
             {
                 // get property
-                string prop = ExpressionManager.GetMatch(filter.Value, @"^(\w|[+\-/%])+").Value;
-                prop = PropManager.MapPlayerProp(prop);
-                object propObj = PropManager.GetPropValue(obj, prop);
-                dynamic propValTemp = propObj.GetType().GetProperty("value").GetValue(propObj);
-                dynamic propVal;
+                string propString = ExpressionManager.GetMatch(filter.Value, @"^(\w|[+\-/%])+").Value;
+                propString = PropManager.MapPlayerProp(propString);
+                object propObj = PropManager.GetPropValue(obj, propString);
+                dynamic propTemp = propObj.GetType().GetProperty("value").GetValue(propObj);
+                dynamic prop;
 
                 // check property on object
-                if (propValTemp is string)
-                {
-                    if (propValTemp == "invalid property")
-                        continue;
-                }
+                if (propTemp is string)
+                    if (propTemp == "invalid property") continue;
 
-                if (propValTemp is CustomTime)
-                    propVal = (propValTemp as CustomTime).AsSeconds();
+                if (propTemp is CustomTime)
+                    prop = (propTemp as CustomTime).AsSeconds();  // explicit cast to see method
                 else
-                    propVal = propValTemp;
+                    prop = propTemp;
 
                 // get operation
                 string op = ExpressionManager.GetMatch(filter.Value, @"[<>=]{1,2}").Value.Trim();
@@ -40,32 +38,32 @@ namespace NHLPlayers
                 // get expression
                 dynamic expTemp = ExpressionManager.GetMatch(filter.Value, @"(\w|\s|[\-\.])+$").Value.Trim();
                 dynamic exp;
-                Type propType = propVal.GetType();
-                expTemp = Convert.ChangeType(expTemp, propType);
+                Type propType = prop.GetType();
+                
 
-                if (expTemp is CustomTime)
-                    exp = (expTemp as CustomTime).AsSeconds();
+                if (prop is CustomTime)
+                    exp = (new CustomTime(expTemp)).AsSeconds();  // explicit cast to see method
                 else
-                    exp = expTemp;
+                    exp = Convert.ChangeType(expTemp, propType);
 
                 // do not allow < > operations of string values
-                if ((op.Contains('<') || op.Contains('>')) && propVal is string) 
+                if ((op.Contains('<') || op.Contains('>')) && prop is string) 
                     continue;
 
                 // dynamic type used to bypass compiler error when comparing two objects
                 switch (op)
                 {
                     case "<":
-                        state = state && (propVal < exp);
+                        state = state && (prop < exp);
                         break;
                     case ">":
-                        state = state && (propVal > exp);
+                        state = state && (prop > exp);
                         break;
                     case "<=": case "=<":
-                        state = state && (propVal <= exp);
+                        state = state && (prop <= exp);
                         break;
                     case ">=": case "=>":
-                        state = state && (propVal >= exp);
+                        state = state && (prop >= exp);
                         break;
                     case "==":
                         state = state && (propVal == exp);
