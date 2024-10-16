0bf399b2c7bc2c04846e48783924c3e3a8ea9e44
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-10-16 14:25:41 -0600

    Removed switch in Player.SetValue

--- NHLPlayers/Player.cs
+++ NHLPlayers/Player.cs
@@ -4,27 +4,27 @@ namespace NHLPlayers
 {
     public class Player
     {
-        public string? Name { get; set; }
-        public List<string?>? Team { get; set; }
-        public string? Pos { get; set; }
-        public int? GP { get; set; }
-        public int? G { get; set; }
-        public int? A { get; set; }
-        public int? PTS { get; set; }            // P changed to PTS
-        public int? Plus_Minus { get; set; }     // +/- changed to Plus_Minus
-        public int? PIM { get; set; }
+        public string Name { get; set; }
+        public List<string> Team { get; set; }
+        public string Pos { get; set; }
+        public int GP { get; set; }
+        public int G { get; set; }
+        public int A { get; set; }
+        public int PTS { get; set; }            // P changed to PTS
+        public int Plus_Minus { get; set; }     // +/- changed to Plus_Minus
+        public int PIM { get; set; }
         public double? P_GP { get; set; }       // P/GP changed to P_GP
-        public int? PPG { get; set; }
-        public int? PPP { get; set; }
-        public int? SHG { get; set; }
-        public int? SHP { get; set; }
-        public int? GWG { get; set; }
-        public int? OTG { get; set; }
-        public int? SOG { get; set; }            // S changed to SOG
-        public double? S_Perc { get; set; }      // S% changed to S_Perc
-        public CustomTime? TOI_GP { get; set; }  // TOI/GP changed to TOI_GP
-        public double? Shifts_GP { get; set; }   // Shifts/GP changed to Shifts_GP
-        public double? FOW_Perc { get; set; }    // FOW% changed to FOW_Perc
+        public int PPG { get; set; }
+        public int PPP { get; set; }
+        public int SHG { get; set; }
+        public int SHP { get; set; }
+        public int GWG { get; set; }
+        public int OTG { get; set; }
+        public int SOG { get; set; }            // S changed to SOG
+        public double S_Perc { get; set; }      // S% changed to S_Perc
+        public CustomTime TOI_GP { get; set; }  // TOI/GP changed to TOI_GP
+        public double Shifts_GP { get; set; }   // Shifts/GP changed to Shifts_GP
+        public double FOW_Perc { get; set; }    // FOW% changed to FOW_Perc
 
         public Player(List<string> data)
         {
@@ -33,40 +33,52 @@ namespace NHLPlayers
 
             for (int i = 0; i < props.Length; i++)
             {
-                SetValue(props[i], data[i], i);
+                SetValue(props[i], data[i]);
             }
         }
 
-        private void SetValue(PropertyInfo prop, string data, int i)
+        private void SetValue(PropertyInfo prop, string data)
         {
-            switch (i)
+            Type propType = prop.PropertyType;
+
+            if (propType == typeof(int))
             {
-                case 0: case 2: // Name, Pos
-                    prop.SetValue(this, data);
-                    break;
-                case 1: // Team
-                    List<string> teams = data.Split(',').ToList();
+                prop.SetValue(this, int.Parse(data));
+                return;
+            }
 
-                    for (int j = 0; j < teams.Count; j++)
-                        teams[j] = teams[j].Trim();
-
-                    prop.SetValue(this, teams);
-                    break;
-                case 9: // P_GP
-                    if (data == "--")
-                        prop.SetValue(this, null);
-                    else
-                        prop.SetValue(this, double.Parse(data));
-                        break;
-                case 17: case 19: case 20: // S_Perc, Shifts_GP, FOW_Perc
+            if (propType == typeof(double) || propType == typeof(double?))
+            {
+                if (data == "--")
+                    prop.SetValue(this, null);
+                else
                     prop.SetValue(this, double.Parse(data));
-                    break;
-                case 18: // TOI_GP
-                    prop.SetValue(this, new CustomTime(data));
-                    break;
-                default: // everything else
-                    prop.SetValue(this, int.Parse(data));
-                    break;
+                return;
+            }
+
+            if (propType == typeof(string))
+            {
+                prop.SetValue(this, data);
+                return;
+            }
+
+            if (propType == typeof(List<string>))
+            {
+                List<string> teams = data.Split(',').ToList();
+
+                for (int j = 0; j < teams.Count; j++)
+                {
+                    teams[j] = teams[j].Trim();
+                }
+
+                prop.SetValue(this, teams);
+                return;
+            }
+
+            if (propType == typeof(CustomTime))
+            {
+                prop.SetValue(this, new CustomTime(data));
+                return;
             }
         }
 
@@ -76,7 +88,8 @@ namespace NHLPlayers
             PropertyInfo[] props = this.GetType().GetProperties();
             foreach (PropertyInfo prop in props)
             {
-                if (prop.Name == "Team")
+                string name = PropManager.MapPlayerProp(prop.Name);
+                if (name == "Team")
                 {
                     string value = "";
                     var list = prop.GetValue(this) as List<string>;
@@ -86,11 +99,11 @@ namespace NHLPlayers
                         if (i + 1 != list.Count)
                             value += ", ";
                     }
-                    propsString += $"{prop.Name}: {value}\n";
+                    propsString += $"{name}: {value}\n";
                 }
                 else
                 {
-                    propsString += $"{prop.Name}: {prop.GetValue(this)}\n";
+                    propsString += $"{name}: {prop.GetValue(this)}\n";
                 }
             }
 
